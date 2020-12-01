5b061416dfc3fdee6155efd363810a8f3c48d6c7
Nikita Miroshnichenko <nikita.mirosha@gmail.com>
2020-12-01 19:26:23 +0300

    Made horizontal scrolling list better. Fixed bugs

---
+++ app/src/main/java/com/insspring/nikita_internship/productlist/ProductAdapter.kt
@@ -0,0 +1,28 @@
+package com.insspring.nikita_internship.productlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.insspring.nikita_internship.R
+import kotlinx.android.synthetic.main.row_post.view.*
+
+class ProductAdapter(val posts: ArrayList<String>) :
+    RecyclerView.Adapter<ProductAdapter.PostsViewHolder>() {
+
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        holder.username.text = posts[position]
+    }
+
+    override fun getItemCount() = posts.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.row_post, parent, false)
+        return PostsViewHolder(view)
+    }
+
+    class PostsViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        val username: TextView = itemview.username
+    }
+}
--- app/src/main/java/com/insspring/nikita_internship/productlist/ProductListActivity.kt
+++ app/src/main/java/com/insspring/nikita_internship/productlist/ProductListActivity.kt
@@ -1,93 +1,28 @@
 package com.insspring.nikita_internship.productlist
 
-import android.animation.ArgbEvaluator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.SnapHelper
 import com.insspring.nikita_internship.R
-
+import kotlinx.android.synthetic.main.activity_product_list.*
 
 class ProductListActivity : AppCompatActivity() {
-
-    lateinit var viewPager: ViewPager
-    lateinit var adapter: Adapter
-    lateinit var models: List<TestModel>
-    var colors: Array<Int>? = null
-    var argbEvaluator = ArgbEvaluator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
 
-        models = ArrayList()
-        (models as ArrayList<TestModel>).add(
-            TestModel(
-                R.drawable.pic_orange,
-                "Orange",
-                "This is Orange"
-            )
-        )
-        (models as ArrayList<TestModel>).add(
-            TestModel(
-                R.drawable.pic_lemon,
-                "Lemon",
-                "This is Lemon"
-            )
-        )
-        (models as ArrayList<TestModel>).add(
-            TestModel(
-                R.drawable.pic_apple,
-                "Apple",
-                "This is Apple"
-            )
-        )
-        (models as ArrayList<TestModel>).add(
-            TestModel(
-                R.drawable.pic_orange,
-                "Orange Again",
-                "This is Orange Again"
-            )
-        )
+        val posts: ArrayList<String> = ArrayList()
+        for (i in 1..10) {
+            posts.add("Card #$i")
+        }
 
-        adapter = Adapter(models, this)
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = adapter
-        viewPager.setPadding(130, 0, 130, 0)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = ProductAdapter(posts)
 
-        val colors_temp = arrayOf(
-            resources.getColor(R.color.colorGray),
-            resources.getColor(R.color.colorPrimaryDark),
-            resources.getColor(R.color.colorOrange),
-            resources.getColor(R.color.colorWhite)
-        )
-
-        colors = colors_temp
-
-        viewPager.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-                if (position < (adapter.getCount() - 1) && position < (colors!!.size - 1)) {
-                    viewPager.setBackgroundColor(
-                        argbEvaluator.evaluate(
-                            positionOffset,
-                            colors?.get(position),
-                            colors?.get(position + 1)
-                        ) as Int
-                    )
-                } else {
-                    viewPager.setBackgroundColor(colors!![colors!!.size - 1])
-                }
-            }
-
-            override fun onPageSelected(position: Int) {
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-            }
-        })
-
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
     }
 }
